1c9b36c38fd8e6c4b6d70d7cfb741f3865f71d86
Maxwell Orth <max.orth96@gmail.com>
2020-12-22 11:14:00 -0800

    Version 1

---
+++ WeaponSprintSensitivityFix.lua
@@ -0,0 +1,186 @@
+
+wssAddon = {}
+wssAddon.name = "WeaponSprintSensitivityFix"
+
+WSS_SPRINT_STATE_SPRINTING_HAS_STAM = 1 -- sprint button is down, player is sprinting (has stamina)
+WSS_SPRINT_STATE_TRYING_SPRINT_NO_STAM = 2 -- sprint button is down, player is not sprinting (out of stamina)
+WSS_SPRINT_STATE_NOT_SPRINTING = 3  -- sprint button is up, and player is not sprinting (duh)
+
+--[[
+    possible state transitions:
+        NOT_SPRINTING -> SPRINTING_HAS_STAM             player pressed sprint while char has stamina
+        SPRINTING_HAS_STAM -> NOT_SPRINTING             player stopped pressing sprint while sprinting
+        SPRINTING_HAS_STAM -> TRYING_SPRINT_NO_STAM     character ran out of stamina while player was pressing sprint
+        TRYING_SPRINT_NO_STAM -> NOT_SPRINTING          player stopped pressing sprint afeter char ran out of stamina while sprinting
+
+    The only way to start sprinting after running out of sprint is to release and re-press the sprint button.
+--]]
+
+wssAddon.weapons_sheathed = true
+wssAddon.sprint_state = WSS_SPRINT_STATE_NOT_SPRINTING
+wssAddon.sprint_button = false
+
+function wssAddon:OnAddOnLoaded(addonName)
+    if addonName == RTIaddon.name then
+        wssAddon:Initialize()
+    end
+end
+
+
+function tokenizeCommand(argString)
+    local args = {}
+    local argTable = { string.match(argString,"^(%S*)%s*(.-)$") }
+        for i,v in pairs(argTable) do
+        if (v ~= nil and v ~= "") then
+            args[i] = string.lower(v)
+        end
+    end
+    return args
+end
+
+function wssAddon:ApplyNormalCameraSettings()
+    SetSetting(SETTING_TYPE_CAMERA, CAMERA_SETTING_SENSITIVITY_THIRD_PERSON, "" .. self.saved_variables.cam_sens.third_person)
+    SetSetting(SETTING_TYPE_CAMERA, CAMERA_SETTING_SENSITIVITY_FIRST_PERSON, "" .. self.saved_variables.cam_sens.first_person)
+    d("Applying normal settings")
+end
+
+function wssAddon:ApplyFasterCameraSettings()
+    SetSetting(SETTING_TYPE_CAMERA, CAMERA_SETTING_SENSITIVITY_THIRD_PERSON, "" .. self.saved_variables.cam_sens.third_person * 2)
+    SetSetting(SETTING_TYPE_CAMERA, CAMERA_SETTING_SENSITIVITY_FIRST_PERSON, "" .. self.saved_variables.cam_sens.first_person * 2)
+    d("Applying sprint settings")
+end
+
+-- sprint combat event does not trigger.
+-- there's no event for weapon sheath/unsheath.
+-- either of those two mean we have to loop to know when state changes.  Fortunately this is o(1) and has no leaks so should not be a perf hit.
+
+function wssAddon:CheckWeaponSprintStatus()
+    local shift = IsShiftKeyDown()
+    local toggledSprintOn = false
+    local toggledSprintOff = false
+    if shift ~= wssAddon.sprint_button then
+        d("toggled sprint")
+        wssAddon.sprint_button = shift
+        toggledSprintOn = shift == true
+        toggledSprintOff = shift == false
+    end
+
+    local weaponStatus = ArePlayerWeaponsSheathed()
+    local weaponsEquipped = false
+    local weaponsUnequipped = false
+    if weaponStatus ~= wssAddon.weapons_sheathed then
+        d("weapon status")
+        weaponsEquipped = weaponStatus == false
+        weaponsUnequipped = weaponStatus == true
+    end
+
+    local startedSprinting = false
+    local stoppedSprinting = false
+
+    if wssAddon.sprint_state == WSS_SPRINT_STATE_NOT_SPRINTING then
+        if toggledSprintOn then
+            local cur, max, effMax = GetUnitPower("player", POWERTYPE_STAMINA)
+            if cur > 0 then
+                wssAddon.sprint_state = WSS_SPRINT_STATE_SPRINTING_HAS_STAM
+                startedSprinting = true
+            end
+        elseif toggledSprintOff then
+            -- d("State machine warning A") -- released sprint button.  Not really a problem.  Happens on ui load/reload.
+            stoppedSprinting = true
+        end
+    elseif wssAddon.sprint_state == WSS_SPRINT_STATE_SPRINTING_HAS_STAM then
+        local cur, max, effMax = GetUnitPower("player", POWERTYPE_STAMINA)
+        if toggledSprintOn then
+            d("SprintCamSens: State machine error B. Notify the addon author!") -- sprint button pressed while already sprinting
+        elseif toggledSprintOff then
+            -- don't care about our stamina, go right to the no sprint state.
+            wssAddon.sprint_state = WSS_SPRINT_STATE_NOT_SPRINTING
+            stoppedSprinting = true
+        elseif cur <= 0 then
+            wssAddon.sprint_state = WSS_SPRINT_STATE_TRYING_SPRINT_NO_STAM
+            stoppedSprinting = true
+        end
+    elseif wssAddon.sprint_state == WSS_SPRINT_STATE_TRYING_SPRINT_NO_STAM then
+        if toggledSprintOn then
+            d("SprintCamSens: State machine error C. Notify the addon author!") -- pressed sprint button when the sprint button should still be down
+            stoppedSprinting = true
+        elseif toggledSprintOff then
+            wssAddon.sprint_state = WSS_SPRINT_STATE_NOT_SPRINTING
+        end
+    end
+
+    if      (stoppedSprinting and not wssAddon.weapons_sheathed)
+        or  (weaponsUnequipped and wssAddon.sprint_state == WSS_SPRINT_STATE_SPRINTING_HAS_STAM)
+    then
+        wssAddon:ApplyNormalCameraSettings()
+    elseif  (startedSprinting and not wssAddon.weapons_sheathed)
+        or  (weaponsEquipped and wssAddon.sprint_state == WSS_SPRINT_STATE_SPRINTING_HAS_STAM)
+    then
+        wssAddon:ApplyFasterCameraSettings()
+    end
+
+    wssAddon.weapons_sheathed = weaponStatus
+
+end
+
+function wssAddon:saveCamSens()
+    local cam_sens = {}
+    cam_sens.third_person = GetSetting(SETTING_TYPE_CAMERA, CAMERA_SETTING_SENSITIVITY_THIRD_PERSON)
+    cam_sens.first_person = GetSetting(SETTING_TYPE_CAMERA, CAMERA_SETTING_SENSITIVITY_FIRST_PERSON)
+    self.saved_variables.cam_sens = cam_sens
+end
+
+function wssAddon:applyCamSens()
+    SetSetting(SETTING_TYPE_CAMERA, CAMERA_SETTING_SENSITIVITY_THIRD_PERSON, "" .. self.saved_variables.cam_sens.third_person)
+    SetSetting(SETTING_TYPE_CAMERA, CAMERA_SETTING_SENSITIVITY_FIRST_PERSON, "" .. self.saved_variables.cam_sens.first_person)
+end
+
+function wssAddon:Initialize()
+    EVENT_MANAGER:RegisterForUpdate(wssAddon.name, 1, wssAddon.CheckWeaponSprintStatus)
+
+    wssAddon.weapons_sheathed = ArePlayerWeaponsSheathed()
+    wssAddon.sprint_button = IsShiftKeyDown()
+
+    SLASH_COMMANDS["/camsens"] = function(argstr)
+        local args = tokenizeCommand(argstr)
+        d(args)
+        if args[1] == "apply" then
+            -- apply cachced sens to game.  Useful if you crash while sens is modified.
+            wssAddon:applyCamSens()
+
+        elseif args[1] == "clean" then
+            -- re-cache the sens settings.  For when we didn't detect a change.
+            wssAddon:saveCamSens()
+        elseif args[1] == "print" then
+            d("Saved cam sens is: " .. wssAddon.saved_variables.cam_sens.third_person .. ", " .. wssAddon.saved_variables.cam_sens.first_person)
+            d("cam sens is: " .. GetSetting(SETTING_TYPE_CAMERA, CAMERA_SETTING_SENSITIVITY_THIRD_PERSON))
+        else
+            d(wssAddon.saved_variables.cam_sens)
+            d("Invalid arguments!  \n\"/camsens apply\" to reapply saved cam sens.\n\"/camsens clean\" to update the cached cam sens")
+        end
+
+    end
+
+    self.saved_variables = ZO_SavedVars:New("CachedCamSens", 1, nil, {})
+    if self.saved_variables.cam_sens == nil then
+        local cam_sens = {}
+        cam_sens.third_person = GetSetting(SETTING_TYPE_CAMERA, CAMERA_SETTING_SENSITIVITY_THIRD_PERSON)
+        cam_sens.first_person = GetSetting(SETTING_TYPE_CAMERA, CAMERA_SETTING_SENSITIVITY_FIRST_PERSON)
+        self.saved_variables.cam_sens = cam_sens
+    end
+
+end
+
+
+EVENT_MANAGER:RegisterForEvent(wssAddon.name, EVENT_ADD_ON_LOADED, wssAddon.OnAddOnLoaded)
+
+
+
+
+
+
+
+
+
+
+
